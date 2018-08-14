9ec88430258018986385f0d1a05b4a2a46951e9e
Aleksey Burger <alekseyburger@gmail.com>
2018-08-14 18:04:44 +0300

    Spark example #01

---
+++ sparkex01/src/main/java/SparkEx01.java
@@ -0,0 +1,15 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+/*
+ Exec: bin/spark-submit --class "SparkEx01" --master local[2] /home/alekseyb/bigdata-workspace/sparkex01/target/spark_ex01-1.0-SNAPSHOT.jar
+ */
+public class SparkEx01 {
+    public static void main(String[] argv) {
+        SparkConf conf = new SparkConf().setAppName("Line_Count");
+        JavaSparkContext ctx = new JavaSparkContext(conf);
+        JavaRDD<String> textLoadRDD = ctx.textFile("/home/alekseyb/spark-2.3.1-bin-hadoop2.7/README.md");
+        System.out.println(textLoadRDD.count());
+    }
+}
